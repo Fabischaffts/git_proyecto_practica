5c58b7a5e93a75c8c9c4ac3b472696d1a05c9137
Martin Vega <martin.vega@osde.com.ar>
2024-05-21 08:43:38 -0300

    se agrega saludo

--- src/ar/edu/utn/pricipal/Principal.java
+++ src/ar/edu/utn/pricipal/Principal.java
@@ -3,7 +3,8 @@ package ar.edu.utn.pricipal;
 public class Principal {
         //comentario martin
 	public static void main(String[] args) {
-	//Esto es un comentario	
+
+		System.System.out.println("Hola mundo!!!");	
 		
 	}
 
