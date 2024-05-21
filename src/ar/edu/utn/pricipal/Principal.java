a582de35759f8ee0b088ac62fc37991f6cda5943
Martin Vega <martin.vega@osde.com.ar>
2024-05-21 08:52:58 -0300

    se traen cambios de main

--- src/ar/edu/utn/pricipal/Principal.java
+++ src/ar/edu/utn/pricipal/Principal.java
@@ -1,7 +1,8 @@
 package ar.edu.utn.pricipal;
 
 public class Principal {
-        //comentario martin
+    //comentario martin
+	//esto es un metodo main
 	public static void main(String[] args) {
 
 		System.System.out.println("Hola mundo!!!");	
