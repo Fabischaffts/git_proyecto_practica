3ccdf643e091855c1841600cd769774cb5ce4dab
Martín Vega <edumarvega@gmail.com>
2024-05-21 08:36:34 -0300

    Se agrega comantarios al metodo main

--- src/ar/edu/utn/pricipal/Principal.java
+++ src/ar/edu/utn/pricipal/Principal.java
@@ -1,7 +1,8 @@
 package ar.edu.utn.pricipal;
 
 public class Principal {
-        //comentario martin
+    //comentario martin
+	//esto es un metodo main
 	public static void main(String[] args) {
 	//Esto es un comentario	
 		
